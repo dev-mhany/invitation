6ebd2b24760a27fe9d6d2c8c85497f6c22540a97
Muhammad Hany <dev.mhany@gmail.com>
2023-12-09 15:39:31 +0200

    first commit

---
+++ src/CountdownTimer.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import "./CountdownTimer.css"; // Ensure you have the CSS file imported
+
+const CountdownTimer = () => {
+  const calculateTimeLeft = () => {
+    const targetDate = new Date("2023-12-21T18:00:00");
+    const targetTime = targetDate.getTime();
+    const difference = targetTime - new Date().getTime();
+
+    if (difference < 0) {
+      // If the difference is negative, the date has passed
+      return null;
+    } else {
+      // Else, calculate the time left
+      return {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    // Set up the interval to tick every second
+    const timer = setTimeout(() => {
+      const newTimeLeft = calculateTimeLeft();
+      setTimeLeft(newTimeLeft);
+    }, 1000);
+
+    // If the timeLeft is null, clear the interval
+    if (!timeLeft) {
+      clearTimeout(timer);
+    }
+
+    // Clear timeout if the component is unmounted
+    return () => clearTimeout(timer);
+  }, [timeLeft]);
+
+  const timerComponents = timeLeft
+    ? Object.keys(timeLeft).map((interval) => {
+        return (
+          <span key={interval} className="time-component">
+            {timeLeft[interval]}{" "}
+            <span className="time-label">{interval.toUpperCase()}</span>
+          </span>
+        );
+      })
+    : "Finally Engaged!";
+
+  return <div>{timerComponents}</div>;
+};
+
+export default CountdownTimer;
---
+++ src/MainPage.js
@@ -0,0 +1,97 @@
+import React, { useRef, useState } from "react";
+import "./MainPage.css";
+import CountdownTimer from "./CountdownTimer";
+import myAudio from "./video.mp3";
+import Button from "@mui/material/Button";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import floralImage from "./122.png"; // Adjust the path as necessary
+
+const MainPage = () => {
+  const audioRef = useRef(null);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [title, setTitle] = useState("Engagement Invitation");
+
+  const handleEnd = () => {
+    setTitle("Finally Engaged");
+  };
+  const playAudio = () => {
+    if (audioRef.current) {
+      audioRef.current
+        .play()
+        .then(() => {
+          setIsButtonClicked(true); // Show the invitation text and background image
+        })
+        .catch((error) => {
+          console.error("Error occurred when trying to play audio:", error);
+        });
+    }
+  };
+
+  return (
+    <div className="main-page">
+      {isButtonClicked && <div className="background-image" />}{" "}
+      {/* This div will show the background image */}
+      <audio ref={audioRef} style={{ display: "none" }}>
+        <source src={myAudio} type="audio/mpeg" />
+      </audio>
+      {!isButtonClicked && (
+        <Button
+          variant="outlined"
+          size="large"
+          startIcon={<HeadphonesIcon />}
+          onClick={playAudio}
+          style={{
+            fontSize: "2em",
+            padding: "10px 20px",
+            cursor: "pointer",
+            zIndex: 1,
+          }}
+        >
+          Play Me
+        </Button>
+      )}
+      {isButtonClicked && (
+        <>
+          <div className="title">{title}</div>
+          <div className="invitation-text">
+            <div className="names">Ahmed & Sohaila</div>
+            <div className="date">Thursday, 21 December 2023</div>
+            <div className="countdown">
+              <CountdownTimer onEnd={handleEnd} />
+            </div>
+            <div className="additional-content">
+              {/* Custom text */}
+              <p className="custom-text">
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                《وَمِنْ آيَاتِهِ أَنْ خَلَقَ لَكُم مِّنْ أَنفُسِكُمْ أَزْوَاجًا
+                لِّتَسْكُنُوا إِلَيْهَا وَجَعَلَ بَيْنَكُم مَّوَدَّةً وَرَحْمه》
+                <br />
+                صدق الله العظيم السلام عليكم ورحمة الله وبركاته. ندعوكم إلى حفل
+                خطوبتنا بحضوركم تكتمل فرحتنا
+              </p>
+              <div className="names">Ahmed & Sohaila</div>
+
+              {/* Decorative image */}
+              <img
+                src={floralImage}
+                alt="Floral decoration"
+                className="decorative-image"
+              />
+
+              {/* Names styled similarly to the example provided */}
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MainPage;
