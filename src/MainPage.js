3330d0a6abe3cf54d9b0e957fa43bcef41098887
Muhammad Hany <dev.mhany@gmail.com>
2023-12-13 10:45:55 +0200

    maps link

--- src/MainPage.js
+++ src/MainPage.js
@@ -14,8 +14,7 @@ const MainPage = () => {
   const [isButtonClicked, setIsButtonClicked] = useState(false);
   const [isImageLoaded, setIsImageLoaded] = useState(false); // New state for image load
   const [title, setTitle] = useState("Engagement Invitation");
-  const mapUrl =
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d166.83325079462048!2d31.742164055535753!3d30.281045694437484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1457fda16b0c4af9%3A0xe0babf9e64ba46d8!2z2YXZiNmEINin2YTYrtin2YY!5e1!3m2!1sen!2seg!4v1702330787603!5m2!1sen!2seg";
+  const mapUrl = "https://maps.app.goo.gl/8S5xJrEYUANQUKXRA";
 
   const handleEnd = () => {
     setTitle("Finally Engaged");
